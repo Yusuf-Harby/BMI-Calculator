3e256724810bba0aced58e6ace45f227428a2dcf
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-05 15:03:08 +0200

    handling the home screen widgets sizes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,230 +10,256 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColor.primary,
       appBar: CustomAppBar(),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColor.selected,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/icons/male_icon.png',
-                        width: 144,
-                        height: 144,
-                      ),
-                      Text(
-                        'Male',
-                        style: TextStyle(
-                          color: AppColor.gray,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColor.unselected,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/icons/female_icon.png',
-                        width: 144,
-                        height: 144,
-                      ),
-                      Text(
-                        'Female',
-                        style: TextStyle(
-                          color: AppColor.gray,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: AppColor.unselected,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          spacing: 25,
+          children: [
+            Expanded(
+              child: Row(
+                spacing: 10,
                 children: [
-                  Text(
-                    'Height',
-                    style: TextStyle(
-                      color: AppColor.gray,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  Text.rich(
-                    TextSpan(
-                      text: '177',
-                      style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColor.selected,
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                      children: [
-                        TextSpan(
-                          text: 'cm',
-                          style: TextStyle(
-                            color: AppColor.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.w500,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icons/male_icon.png',
+                            width: 144,
+                            height: 144,
                           ),
-                        ),
-                      ],
+                          Text(
+                            'Male',
+                            style: TextStyle(
+                              color: AppColor.gray,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                  Slider(
-                    value: 60,
-                    onChanged: (val) {},
-                    min: 20,
-                    max: 200,
-                    activeColor: AppColor.secondary,
-                    inactiveColor: AppColor.white,
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColor.unselected,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icons/female_icon.png',
+                            width: 144,
+                            height: 144,
+                          ),
+                          Text(
+                            'Female',
+                            style: TextStyle(
+                              color: AppColor.gray,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColor.unselected,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Weight',
-                        style: TextStyle(
-                          color: AppColor.gray,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppColor.unselected,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    SizedBox(height: 27),
+                    Text(
+                      'Height',
+                      style: TextStyle(
+                        color: AppColor.gray,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
                       ),
-                      Text(
-                        '60',
+                    ),
+                    SizedBox(height: 6),
+                    Text.rich(
+                      TextSpan(
+                        text: '177',
                         style: TextStyle(
                           color: AppColor.white,
                           fontSize: 40,
                           fontWeight: FontWeight.bold,
                         ),
-                      ),
-                      Row(
                         children: [
-                          FloatingActionButton(
-                            onPressed: () {},
-                            backgroundColor: AppColor.gray,
-                            shape: CircleBorder(),
-                            child: Text(
-                              '-',
-                              style: TextStyle(
-                                color: AppColor.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          FloatingActionButton(
-                            onPressed: () {},
-                            backgroundColor: AppColor.gray,
-                            shape: CircleBorder(),
-                            child: Text(
-                              '+',
-                              style: TextStyle(
-                                color: AppColor.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600,
-                              ),
+                          TextSpan(
+                            text: 'cm',
+                            style: TextStyle(
+                              color: AppColor.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w500,
                             ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                    Slider(
+                      value: 60,
+                      onChanged: (val) {},
+                      min: 20,
+                      max: 200,
+                      activeColor: AppColor.secondary,
+                      inactiveColor: AppColor.white,
+                    ),
+                  ],
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: AppColor.unselected,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Age',
-                        style: TextStyle(
-                          color: AppColor.gray,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      Text(
-                        '21',
-                        style: TextStyle(
-                          color: AppColor.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          FloatingActionButton(
-                            onPressed: () {},
-                            backgroundColor: AppColor.gray,
-                            shape: CircleBorder(),
-                            child: Text(
-                              '-',
-                              style: TextStyle(
-                                color: AppColor.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          FloatingActionButton(
-                            onPressed: () {},
-                            backgroundColor: AppColor.gray,
-                            shape: CircleBorder(),
-                            child: Text(
-                              '+',
-                              style: TextStyle(
-                                color: AppColor.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: Row(
+                spacing: 10,
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColor.unselected,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          Text(
+                            'Weight',
+                            style: TextStyle(
+                              color: AppColor.gray,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '60',
+                            style: TextStyle(
+                              color: AppColor.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 21),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {},
+                                backgroundColor: AppColor.gray,
+                                shape: CircleBorder(),
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    color: AppColor.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              FloatingActionButton(
+                                onPressed: () {},
+                                backgroundColor: AppColor.gray,
+                                shape: CircleBorder(),
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    color: AppColor.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: AppColor.unselected,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(height: 15),
+                          Text(
+                            'Age',
+                            style: TextStyle(
+                              color: AppColor.gray,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '21',
+                            style: TextStyle(
+                              color: AppColor.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 21),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {},
+                                backgroundColor: AppColor.gray,
+                                shape: CircleBorder(),
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    color: AppColor.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                              FloatingActionButton(
+                                onPressed: () {},
+                                backgroundColor: AppColor.gray,
+                                shape: CircleBorder(),
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    color: AppColor.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: MaterialButton(
-          onPressed: (){},
+        onPressed: () {},
         color: AppColor.secondary,
         minWidth: double.infinity,
         height: 100,
@@ -242,7 +268,7 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(
             color: AppColor.white,
             fontSize: 32,
-            fontWeight: FontWeight.w600
+            fontWeight: FontWeight.w600,
           ),
         ),
       ),
